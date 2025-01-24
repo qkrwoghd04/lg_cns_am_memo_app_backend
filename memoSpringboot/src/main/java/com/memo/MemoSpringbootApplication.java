953d218a45386206af5b1a9d7fe238709c863a87
JDEV <qkrwoghd04@naver.com>
2025-01-24 15:07:03 +0900

    start spring

---
+++ memoSpringboot/src/main/java/com/memo/MemoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoSpringbootApplication.class, args);
+	}
+
+}
---
+++ memoSpringboot/src/test/java/com/memo/MemoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
